1f23ad76c84eab85084224b4926918ff67a91e21
David Ortiz <do559513@gmail.com>
2018-06-13 14:11:04 -0600

    Documento y Rangos en matricula principal terminado. Agregar cambios en modal de integrante ya matriculado.

--- php/guardarIntegrante.php
+++ php/guardarIntegrante.php
@@ -22,6 +22,27 @@ $rango3=$_POST["phpRango3"];
 $rango4=$_POST["phpRango4"];
 $rango5=$_POST["phpRango5"];
 $rango6=$_POST["phpRango6"];
+
+if($rango1 == ""){
+    $rango1 = 0;
+}
+
+if($rango2 == ""){
+    $rango2 = 0;
+}
+if($rango3 == ""){
+    $rango3 = 0;
+}
+if($rango4 == ""){
+    $rango4 = 0;
+}
+if($rango5 == ""){
+    $rango5 = 0;
+}
+if($rango6 == ""){
+    $rango6 = 0;
+}
+
 $documentos=$_POST["phpDocumentos"];
 $RespuestaDocumentos=$_POST["phpRespuestaDocumentos"];
 
@@ -46,6 +67,15 @@ if($query_ver >0){
 $query = mysqli_query($enlace,"insert into integrantes (promo_cordero,num_identidad,nombre_integrante,fecha_cumple,cel,tel,estado_civil,sexo,trasporte,direccion,areas,apellidoCasada,status,fecha_registro,correlativo,documentosRespuesta,documentosPendientes) values 
 	(".$PromCorderitos.",'".$Identidad."','".$NombreMayus."','".$FechaCumpleanos."','".$Tel1."','".$Tel2."','".$EstadoCivil."','".$Genero."','".$Transporte."','".$Direccion."','".$Areas."','".$ApeCasada."','1','".$fechaentrada."',".$corrNew.",'".$RespuestaDocumentos."','".$documentos."')");
 
+
+$total = $rango1+$rango2+$rango3+$rango4+$rango5+$rango6;
+$encontrarIdIntegrante = mysqli_query($enlace,"select idintegrante from integrantes WHERE num_identidad = '".$Identidad."'");
+$arrayEncontrarIdIntegrante= mysqli_fetch_array($encontrarIdIntegrante,MYSQLI_ASSOC);
+$idIntegrante = $arrayEncontrarIdIntegrante["idintegrante"];
+
+$queryInsertRangos = mysqli_query($enlace,"insert into rangos (rangos.idIntegrante,rangos.`0-2`,rangos.`2-3`,rangos.`4-5`,rangos.`6-7`,rangos.`8-11`,rangos.otros,rangos.total) 
+values 
+('".$idIntegrante."','".$rango1."','".$rango2."','".$rango3."','".$rango4."','".$rango5."','".$rango6."','".$total."')");
 
 
  if (mysqli_affected_rows($enlace)>0) {
